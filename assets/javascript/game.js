c1d6f0e6a8312dae5db6f5d641ba03c20aecce9a
Benjamin Spille <Benjamin Spille>
2017-02-05 21:19:04 -0500

    completed the start buttom to selector function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,50 @@
 //vars and functions go here
 game = {
 	field: $("#mapArea"),
+
 	zones: ["selection", "enemy", "combat", "graveyard",
 			"attacker", "defender"],
+
 	characters: ["char_1", "char_2", "char_3", "char_4"],
+
 	buttons: ["start", "attack"],
+
 	player: [],
+
 	enemy: [],
+
 	HP: [],
+
 	atk: [],
+
 	cntAtk: [],
 	
-	load: function() {
-			var newBtn = $("<button>" + this.buttons[0] + "</button>");
-			newBtn.attr("id", "startBtn");
-			this.field.append(newBtn);
-	},
-	start: function() {
-			$("startBtn").on("click", function() {
-				this.field.empty();
-				var newDiv = $("<div>");
-				newDiv.attr("id", this.zones[0])
+	load: 	function() {
+				var newBtn = $("<button>" + this.buttons[0] + "</button>");
+				newBtn.attr("id", "startBtn");
+				this.field.append(newBtn);
+			},
 
-			});
-	}
+	start: 	function() {
+				game.field.empty();
+				var newDiv = $("<div>");
+				newDiv.attr("id", game.zones[0]);
+			
+				for (var i = 0; i < game.characters.length; i++) {
+					var newAvatar = $("<div>");
+					var newChara = $("<img>");
+					newAvatar.addClass("avatar");
+					newAvatar.attr("id", "chara_" + [i]);
+					newAvatar.attr("attack", game.atk[i]);
+					newAvatar.attr("counter-attack", game.cntAtk[i]);
+					newAvatar.attr("hit-points", game.HP[i]);
+					newChara.addClass("character");
+					newChara.attr("alt", game.characters[i]);
+					newAvatar.append(newChara);
+					game.field.append(newAvatar);
+				};
+				game.field.append(newDiv);
+			}
 	// player selection function on click
 	// remaining characters are set as enemies and moved to zone
 	// player chooses the order to fight one at a time
@@ -39,4 +60,8 @@ game = {
 // event functions go here
 $(document).ready(function() {
 	game.load();
+	$("#startBtn").on("click", function() {
+		game.start();
+	});
+
 });
