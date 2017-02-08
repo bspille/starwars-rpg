58d8d0f74581d1d1255e955041ac6c94268f3436
Benjamin Spille <Benjamin Spille>
2017-02-08 15:28:17 -0500

    completed stat generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ game = {
 	zones: ["selection", "enemy", "combat", "graveyard"],
 
 	characters: ["char_1", "char_2", "char_3", "char_4"],
+	enemy: ["char_1", "char_2", "char_3", "char_4"],
 
 	charaImage: [],
 
@@ -12,18 +13,21 @@ game = {
 
 	player: [],
 
-	enemy: [],
 
-	HP: [],
 
-	atk: [],
+	HP: ["100", "110", "120", "130"],
+	hpStat: [],
 
-	cntrAtk: [],
-	
+	atk: ["6", "8", "10", "12"],
+	atkStat: [],
+
+	cntrAtk: ["7", "9", "11", "13"],
+	cntrAtkStat: [],	
+
 	load: 	function() {
-				var newBtn = $("<button>" + this.buttons[0] + "</button>");
+				var newBtn = $("<button>" + game.buttons[0] + "</button>");
 				newBtn.attr("id", "startBtn");
-				this.field.append(newBtn);
+				game.field.append(newBtn);
 				$("#startBtn").on("click", function() {
 					game.start();
 				});
@@ -33,36 +37,35 @@ game = {
 				game.field.empty();
 				var newDiv = $("<div>");
 				newDiv.attr("id", game.zones[0]);
-			
 				for (var i = 0; i < game.characters.length; i++) {
 					var newBtn = $("<button>");
 					var newImg = $("<img>");
 					newImg.attr("class", "chara");
 					newImg.attr("alt", game.characters[i]);
-					newImg.attr("src", "asset/images/" + game.charaImage[i]);
+					// newImg.attr("src", "asset/images/" + game.charaImage[i]);
 					newBtn.append(newImg);
 					newBtn.attr("class", "selBtn");
-					newBtn.attr("name", game.characters[i]);
-					newDiv.append(newBtn);
-				};
+					newBtn.val(game.characters[i]);
+					newDiv.append(newBtn);				
+				};							
 				game.field.append(newDiv);
-				$(".selBtn").on("click", function() {
-					game.player	= ($(this).attr("name"));
-					game.setparty();
-				});
+				game.setparty();
 			},
 
-	setparty: 	function () {
-					var n = game.characters.indexOf(game.player);
-					game.enemy = game.characters
-					game.enemy.splice(n,1);
-					game.genmap();
+	setparty: 	function() {				
+					$(".selBtn").on("click", function() {
+						game.player	= ($(this).val());
+						var n = game.enemy.indexOf(game.player);
+						game.enemy.splice(n,1);							
+						game.genmap();						
+					});
 				},
 
 	genmap: function() {
 				game.field.empty();
 				for (var i = 0; i < game.zones.length; i++) {
 					var newDiv = $("<div>");
+					
 					newDiv.attr("id", game.zones[i]);
 					newDiv.attr("class", "quadrant");
 					game.field.append(newDiv);
@@ -73,14 +76,15 @@ game = {
 	fieldplyr: 	function() {
 					var newDiv = $("<div>");
 					var newImg = $("<img>");
+					
 					newImg.attr("alt", game.player);
+					newImg.attr("id", game.characters.indexOf(game.player));
+					
 					newDiv.append(newImg);
 					newDiv.addClass("avatar");
 					newDiv.attr("id", game.player);
-					newDiv.attr("atk", "");
-					newDiv.attr("cntrAtk", "");
-					newDiv.attr("HP", "");
 					$("#selection").append(newDiv);
+					
 					game.fieldenemy();									
 				},
 
@@ -88,22 +92,47 @@ game = {
 					for (i = 0; i < game.enemy.length; i++) {
 						var newBtn = $("<button>");
 						var newImg = $("<img>");
+
 						newImg.attr("alt", game.enemy[i]);
+						newImg.attr("id", "image_" + game.characters.indexOf(game.enemy[i]));
+						
 						newBtn.append(newImg);
 						newBtn.addClass("avatar");
 						newBtn.attr("id", game.enemy[i]);
-						newBtn.attr("atk", "");
-						newBtn.attr("cntrAtk", "");
-						newBtn.attr("HP", "");
+						
 						$("#enemy").append(newBtn);
 					}
-				}
+					game.genStats();
+				},
 
+	genStats: 	function() {
+					game.hpStat = game.HP;
+					game.atkStat = game.atk;
+					game.cntrAtkStat = game.cntrAtk;
 
-	// link image and random attributes
-	// display hp
-	// random placement of atk cntatk and hp
-	// danger indicator
+					for (var i = 0; i < game.characters.length; i++) {
+						var apply = $("#" + game.characters[i]);
+				
+						var hpl = game.hpStat.length - 1;
+						var rh = (Math.round(Math.random() * hpl));
+						apply.attr("HP", game.HP[rh]);
+						console.log("rh "+ rh);
+						game.hpStat.splice(rh,1);
+						
+						var atkl = game.atkStat.length - 1;						
+						var ra = (Math.round(Math.random() * atkl));
+						console.log("ra " + ra);
+						apply.attr("Atk", game.atk[ra]);
+						game.atkStat.splice(ra,1);
+				
+						var cntrAtkl = game.cntrAtkStat.length - 1;						
+						var rc = (Math.round(Math.random() * cntrAtkl));
+						console.log("rc " + rc);
+						apply.attr("cntrAtk", game.cntrAtk[rc]);
+						game.cntrAtkStat.splice(rc,1);
+					}
+				},
+	
 	// player chooses the order to fight one at a time
 	// enemies are move to a defender area for combat and grave yard when killed
 	// HP displayed on bottom of the picture
@@ -112,6 +141,8 @@ game = {
 	// each player attack increases attack by base atk counters base only
 	// HP atk and cntatk sets must differ for each character
 	// players must be able to win or lose and selection must not depend on the strongest character
+	// display images
+	// display hp
 }
 
 // event functions go here
